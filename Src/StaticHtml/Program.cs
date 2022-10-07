5f17b5d40925935cc236fbdc6d24425ad7e8b8b6
axunonb <nb@axuno.net>
2022-10-07 15:17:07 +0200

    Migrate to ASP.NET project with static content

---
+++ Src/StaticHtml/Program.cs
@@ -0,0 +1,31 @@
+﻿//
+// Copyright easy-does-it.de project maintainers and contributors.
+// Licensed under the MIT license.
+//
+
+namespace StaticHtml
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDefaultFiles(); // must come before UseStaticFiles
+            app.UseStaticFiles();
+        }
+    }
+}
